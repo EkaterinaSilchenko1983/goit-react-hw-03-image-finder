c26a6f4ea02905698c5d2e436f4c8aca33ad46c4
Ekaterina Silchenko <maksil.17@gmail.com>
2022-12-12 22:38:59 +0200

    new commit with gallery

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import { Button } from './Button/Button';
 import { GlobalStyle } from './GlobalStyle';
 import { Searchbar } from './Searchbar/Searchbar';
--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,4 +1,4 @@
-import { PropTypes } from 'prop-types';
+// import { PropTypes } from 'prop-types';
 export const ImageGalleryItem = ({ image }) => {
   return (
     <li className="gallery-item" id={image.id}>
