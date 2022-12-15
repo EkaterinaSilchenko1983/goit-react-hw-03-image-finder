d4d6d8c5bf9f5acaad28bd60eeab9d2b6cdfbc01
Ekaterina Silchenko <maksil.17@gmail.com>
2022-12-15 02:19:51 +0200

    add width and height

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -13,6 +13,11 @@ export const Overlay = styled.div`
 `;
 
 export const ModalImage = styled.div`
-  max-width: calc(100vw - 48px);
-  max-height: calc(100vh - 24px);
+  position: absolute;
+  top: 0;
+
+  left: 15%;
+  width: 70%;
+  height: 70%;
+  object-fit: contain;
 `;
